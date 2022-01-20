bfa28b27005b89097d4efcb02d6fbaf523899c7f
DMITRIY <ilschukdemo@hotmail.com>
2022-01-20 21:08:46 +0300

    Задание 1

---
+++ CSharpBegin_2_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGBBegin_2_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите минимальную температуру: ");
+            Decimal.TryParse(Console.ReadLine(), out decimal minTemperature);
+            Console.Write("Введите максимальную температуру: ");
+            Decimal.TryParse(Console.ReadLine(), out decimal maxTemperature);
+
+            decimal avrgTemperature = (maxTemperature + minTemperature) / 2;
+
+            Console.WriteLine("Средняя температура равна: {0}", avrgTemperature.ToString());
+
+        }
+    }
+}
